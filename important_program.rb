b9282469220573635ee7addf3edfd8aec4620bb0
Rachel Guerin <rachel.guerin23@gmail.com>
2016-02-04 11:48:45 +0100

    changes to important_program.rb

--- important_program.rb
+++ important_program.rb
@@ -1,2 +1,12 @@
-puts "Hi there! What this program does is really important."
-puts "MODIFY THIS PROGRAM AT YOUR OWN PERIL."
+class ImportantThing
+	def initialize(thing)
+		@thing = thing
+	end
+
+	def thing
+		return "You can't have #{thing}. It's too important."
+	end
+end
+
+pizza = ImportantThing.new("pizza")
+puts pizza.thing
